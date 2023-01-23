414c07cae2d98c224092da315a7a41237c485f6d
Nick Corbin <cnick1@vt.edu>
2023-01-23 14:03:10 -0800

    Added blank spots in code for new Q terms

---
+++ approxFutureEnergyQB.m
@@ -0,0 +1,167 @@
+function [w] = approxFutureEnergyQB(A,N,B,Q,C,eta,d,verbose)
+%  Calculates a polynomial approximation to the future energy function
+%  for a quadratic system.
+%
+%  w = approxFutureEnergyQB(A,N,B,Q,C,eta,d) 
+%
+%  Computes a degree d polynomial approximation to the future energy function 
+%
+%          E^+(x) = 1/2 ( w{2}'*kron(x,x) + ... + w{d}'*kron(.. x) )
+%
+%  for the polynomial system
+%
+%    \dot{x} = Ax + Bu + N*kron(x,x) + Q*kron(x,u)
+%          y = Cx
+%
+%  where eta = 1-gamma^(-2), gamma is the parameter in the algebraic Riccati
+%  equation for W2,
+%
+%    A'*W2 + W2*A - eta*W2*B*B'*W2 + C'*C = 0,
+%  
+%  and in the subsequent linear systems arising from the Future H-infinity 
+%  Hamilton-Jacobi-Bellman Partial Differential Equation.
+%
+%  Note that w2 = vec(W2).  Details are in Section III.B of reference [1].
+%
+%  Requires the following functions from the KroneckerTools repository:
+%      KroneckerSumSolver
+%      kronMonomialSymmetrize
+%      LyapProduct
+%
+%  Author: Jeff Borggaard, Virginia Tech
+%
+%  License: MIT
+%
+%  Reference: [1] Nonlinear balanced truncation: Part 1--Computing energy 
+%             functions, by Kramer, Gugercin, and Borggaard, arXiv:2209.07645.
+%             [2] Nonlinear balanced truncation for quadratic bilinear
+%             systems, by Nick Corbin ... (in progress).
+%
+%             See Algorithm 1 in [1].
+%
+%  Part of the NLbalancing repository.
+%%
+
+  if (nargin<7)
+    verbose = false;
+  end
+
+  n = size(A,1);   % A should be n-by-n
+  m = size(B,2);   % B should be n-by-m
+  p = size(C,1);   % C should be p-by-n
+
+  % Create a vec function for readability
+  vec = @(X) X(:);
+
+
+  %% k=2 case
+  R = eye(m)/eta;
+
+  if ( eta>0 )
+    [W2] = icare(A,B,(C.'*C),R);
+    if ( isempty(W2) && verbose )
+      warning('approxFutureEnergyQB: icare couldn''t find stabilizing solution')
+    end
+    
+  elseif ( eta<0 )
+    [W2] = icare(A,B,(C.'*C),R,'anti');
+    
+    if ( isempty(W2) && verbose )
+      warning('approxFutureEnergyQB: icare couldn''t find stabilizing solution')
+      warning('approxFutureEnergyQB: using the hamiltonian')
+      [~,W2,~] = hamiltonian(A,B,C.'*C,R,true);
+    end
+    
+  else % eta==0
+    [W2] = lyap(A.',(C.'*C));
+    
+  end
+
+  if (isempty(W2))
+    error('approxFutureEnergyQB: Can''t find a stabilizing solution')
+  end
+  
+  %  Check the residual of the Riccati/Lyapunov equation
+  if (verbose)
+    RES = A'*W2 + W2*A - eta*(W2*B)*(B'*W2) + C'*C ;
+    fprintf('The residual of the Riccati equation is %g\n',norm(RES,'inf'));
+  end
+
+  %  Reshape the resulting quadratic coefficients
+  w{2} = vec(W2);
+
+  %% k=3 case
+  if ( d>2 ) % set up the generalized Lyapunov solver
+    W2BB = W2*(B*B.');
+    Acell = cell(1,d);
+    for i=1:d % Pre-computing the left-hand-sides of (46) in [1]
+      Acell{i} = A.'-eta*W2BB; 
+    end
+
+    b = -LyapProduct(N.',w{2},2);
+    [w{3}] = KroneckerSumSolver(Acell(1:3),b,3); % Solve Ax=b for k=3
+    
+    [w{3}] = kronMonomialSymmetrize(w{3},n,3); % Symmetrize w3
+  end
+  
+  %% k>3 cases (up to d)
+  BWk = cell(1,d-1); % Pre-compute B.'*Wi for all the i we need
+  if ( d>3 )
+
+    for k=4:d
+      BWk{k-1} = B.'*reshape(w{k-1},n,n^(k-2));
+
+      b = -LyapProduct(N.',w{k-1},k-1); % Pre-compue all the L(N') terms
+      
+      % Now add all the terms from the 'B' sum by looping through the i and j
+      for i=3:(k+1)/2
+        j   = k+2-i;
+        tmp = BWk{i}.'*BWk{j};
+        b   = b + 0.25*eta*i*j*(vec(tmp) + vec(tmp.'));
+      end
+
+      if (mod(k,2)==0) % k+2 is even
+        i   = (k+2)/2; 
+        j   = i;
+        tmp = BWk{i}.'*BWk{j};
+        b   = b + 0.25*eta*i*j*vec(tmp);
+      end
+      
+      % Now add the linear Q terms by iterating through the first sum
+      for i=3:(k)/2
+        j   = k+1-i;
+        %
+      end
+
+      if (mod(k+1,2)==0) % k+1 is even
+        i   = (k+1)/2; 
+        j   = i;
+        %
+      end
+      
+      
+      % Now add the quadratic Q terms by iterating through the second sum
+      for i=3:(k-1)/2
+        j   = k-i;
+        %
+      end
+
+      if (mod(k,2)==0) % k is even
+        i   = (k)/2; 
+        j   = i;
+        %
+      end
+      
+      
+      
+      
+      
+      
+
+      [w{k}] = KroneckerSumSolver(Acell(1:k),b,k);
+
+      [w{k}] = kronMonomialSymmetrize(w{k},n,k);
+    end
+  end
+
+end
--- examplesForPaper1.m
+++ examplesForPaper1.m
@@ -11,10 +11,10 @@ addpath('utils')
 
 %%  runExample2 produces the plots for Fig. 2.  
 %  For Table 1, please use energyFunctionValidation in the tests directory.
-%[v,w] = runExample2(7,true,true,6);
+[v,w] = runExample2(7,true,true,6);
 
 %%  runExample3 produces Tables II, III, and IV.
 %runExample3
 
 %%  runExample4 produces Tables V and VI.
-runExample4
+% runExample4
