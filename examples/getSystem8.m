295809c1054fb26c27863dfe03ad6a18117742e9
Nick Corbin <cnick1@vt.edu>
2023-03-14 01:09:56 -0700

    Added polynomial input to beam model

--- examples/getSystem8.m
+++ examples/getSystem8.m
@@ -1,4 +1,4 @@
-function [A, B, C, N, Q, f, g, h] = getSystem8(numElements, varargin)
+function [A, B, C, N, Q, f, g, h] = getSystem8(numElements, actuatorConfiguration, rotaryInertiaBool, varargin)
 %getSystem8  Generates a cubic system for testing energy functions.
 %            The system is a finite element model for a nonlinear (due to
 %            von Karman strains) Euler-Bernoulli Beam.
@@ -11,9 +11,19 @@ function [A, B, C, N, Q, f, g, h] = getSystem8(numElements, varargin)
 
 vec = @(X) X(:);
 
-if nargin == 0
+if nargin < 1
   numElements = 3;
 end
+
+if nargin < 2
+  actuatorConfiguration = 1;
+end
+
+if nargin < 3
+  rotaryInertiaBool = false;
+end
+
+
 
 %% Define beam geometry and properties
 BeamLength = 1; % length of beam
@@ -21,6 +31,7 @@ ElasticModulus = 210e9; % Young's modulus
 CrossSecArea = 1e-1; % cross-sectional area
 MomOfInertia = 1e-2; % moment of inertia
 density = 8000; % density
+delta = 0.1; % Cable attachment distance from centerline of beam if actuatorConfiguration = 2
 
 % Define element properties
 numNodes = numElements + 1; % number of nodes
@@ -41,7 +52,7 @@ M1E = density * CrossSecArea * elementLength / 420 * ...
  70, 0, 0, 140, 0, 0;
  0, 54, 13 * elementLength, 0, 156, -22 * elementLength;
  0, -13 * elementLength, -3 * elementLength ^ 2, 0, -22 * elementLength, 4 * elementLength ^ 2] ...
-  + density * MomOfInertia / (30 * elementLength) * ...
+  + rotaryInertiaBool*density * MomOfInertia / (30 * elementLength) * ...
   [0, 0, 0, 0, 0, 0;
  0, 36, 33 * elementLength, 0, -36, 3 * elementLength;
  0, 3 * elementLength, 4 * elementLength ^ 2, 0, -3 * elementLength, -elementLength ^ 2;
@@ -191,10 +202,65 @@ for i = 0:numElements - 1 % start from zero so you don't have to subtract 1 ever
 end
 
 %% RHS
-RB = sparse(TotalDOFs, 2);
-RB(TotalDOFs - 1, 1) = 1;
-RB(TotalDOFs - 2, 2) = 1;
-
+switch actuatorConfiguration
+    case 1
+        RB0 = sparse(TotalDOFs, 2);        
+        RB1 = sparse(TotalDOFs, 2*TotalDOFs);
+        RB2 = sparse(TotalDOFs, 2*TotalDOFs^2); 
+        RB3 = sparse(TotalDOFs, 2*TotalDOFs^3);  
+        
+        RB0(TotalDOFs - 2, 2) = 1; % Force in x direction
+        RB0(TotalDOFs - 1, 1) = 1; % Force in y direction
+        RB0(TotalDOFs, :) = 0;     % Moment in z direction
+        
+    case 2
+        RB0 = sparse(TotalDOFs, 2);
+        RB1 = sparse(TotalDOFs, 2*TotalDOFs);
+        RB2 = sparse(TotalDOFs, 2*TotalDOFs^2); 
+        RB3 = sparse(TotalDOFs, 2*TotalDOFs^3);
+        
+        % x     or u     -> (TotalDOFs - 2)
+        % y     or v     -> (TotalDOFs - 1)
+        % theta or dv/dx -> (TotalDOFs)
+        
+        % Order-0 terms
+        RB0(TotalDOFs-2, 1) = -1;     % Force in x direction from cable 1
+        RB0(TotalDOFs-2, 2) = -1;     % Force in x direction from cable 2
+        RB0(TotalDOFs-1, 1) =  0;     % Force in y direction from cable 1
+        RB0(TotalDOFs-1, 2) =  0;     % Force in y direction from cable 2
+        RB0(TotalDOFs  , 1) =  delta; % Moment in z direction from cable 1
+        RB0(TotalDOFs  , 2) = -delta; % Moment in z direction from cable 2
+        
+        % Order-1 terms (only appear in transverse direction)
+        RB1(TotalDOFs-1, 2*(TotalDOFs-1)-1) = -1; % Force in y direction from cable 1 xn-1 u1
+        RB1(TotalDOFs-1, 2*(TotalDOFs-1))   = -1; % Force in y direction from cable 2 xn-1 u2
+        
+        % Order-2 terms
+        RB2(TotalDOFs-2, 2*(TotalDOFs-1)^2-1)             =  1/(2*L^2);         % Force in x direction from cable 1
+        RB2(TotalDOFs-2, 2*(TotalDOFs-1)^2)               =  1/(2*L^2);         % Force in x direction from cable 2
+        RB2(TotalDOFs-1, 2*(TotalDOFs-1)*(TotalDOFs-2)-1) =  1/L^2;             % Force in y direction from cable 1 
+        RB2(TotalDOFs-1, 2*(TotalDOFs-1)*(TotalDOFs-2))   =  1/L^2;             % Force in y direction from cable 2 
+        RB2(TotalDOFs  , 2*(TotalDOFs-1)^2-1)             = -delta/(2*L^2);     % Moment in z direction from cable 1 
+        RB2(TotalDOFs  , 2*(TotalDOFs-1)^2)               =  delta/(2*L^2);     % Moment in z direction from cable 2 
+        
+        % x1x1u1 x1x1u2 x1x2u1 x1x2u2 x1x3u1 x1x3u2 ..... xnx1u1 xnx1u2 ... xnxnu1 xnxnu2
+        % Need (xn-1)(xn-1) u1 = -1
+        %      (xn-1)(xn-1) u2 = -1
+        %
+        % and (xn-1)(xn-2) u1 = -1
+        %     (xn-1)(xn-2) u2 = -1
+        %                               (or (xn-2)(xn-1) u1 = -1
+        
+        % Order-3 terms TODO !!! NOT CORRECT
+        RB3(TotalDOFs-2, 2*(TotalDOFs-2)*(TotalDOFs-1)^2-1) = -1/L^3;         % Force in x direction from cable 1
+        RB3(TotalDOFs-2, 2*(TotalDOFs-2)*(TotalDOFs-1)^2)   = -1/L^3;         % Force in x direction from cable 2
+        RB3(TotalDOFs-1, 2*(TotalDOFs-1)*(TotalDOFs-2)^2-1) = -1/L^3;         % Force in y direction from cable 1 part1
+        RB3(TotalDOFs-1, 2*(TotalDOFs-1)*(TotalDOFs-2)^2)   = -1/L^3;         % Force in y direction from cable 2 part1
+        RB3(TotalDOFs-1, 2*(TotalDOFs-1)^3-1)               =  1/(2*L^3);     % Force in y direction from cable 1 part2
+        RB3(TotalDOFs-1, 2*(TotalDOFs-1)^3)                 =  1/(2*L^3);     % Force in y direction from cable 2 part2
+        RB3(TotalDOFs  , 2*(TotalDOFs-2)*(TotalDOFs-1)^2-1) =  delta/L^3;     % Moment in z direction from cable 1 
+        RB3(TotalDOFs  , 2*(TotalDOFs-2)*(TotalDOFs-1)^2)   = -delta/L^3;     % Moment in z direction from cable 2 
+end
 %% Impose boundary conditions
 fixedDOFs = [1, 2, 3];
 freeDOFs = setdiff(1:TotalDOFs, fixedDOFs);
@@ -208,7 +274,8 @@ freeDOFs = setdiff(1:TotalDOFs, fixedDOFs);
 % Reduced system method
 K1G = K1G(freeDOFs, freeDOFs);
 M1G = M1G(freeDOFs, freeDOFs);
-RB = RB(freeDOFs, :);
+RB0 = RB0(freeDOFs, :);
+RB1 = RB1(freeDOFs, [freeDOFs, freeDOFs+TotalDOFs]);
 
 % K2G could clean up
 fixedDOFsSquared = vec(fixedDOFs.' + (0:TotalDOFs:TotalDOFs^2-1));
@@ -216,6 +283,9 @@ fixedDOFsSquared = unique([fixedDOFsSquared; vec((1:TotalDOFs).' + (fixedDOFs-1)
 
 freeDOFsSquared = setdiff(1:TotalDOFs^2, fixedDOFsSquared);
 K2G = K2G(freeDOFs,freeDOFsSquared);
+RB2 = RB2(freeDOFs, [freeDOFsSquared, freeDOFsSquared+TotalDOFs^2]);
+
+
 
 % K3G could clean up
 fixedDOFsCubed = vec(vec((fixedDOFs-1)*TotalDOFs+[1:TotalDOFs].') + (0:TotalDOFs^2:TotalDOFs^3-1));
@@ -225,6 +295,7 @@ fixedDOFsCubed = sort(unique(fixedDOFsCubed));
 
 freeDOFsCubed = setdiff(1:TotalDOFs^3, fixedDOFsCubed);
 K3G = K3G(freeDOFs,freeDOFsCubed);
+RB3 = RB3(freeDOFs, [freeDOFsCubed, freeDOFsCubed+TotalDOFs^3]);
 
 
 D1G = 0.00001 * M1G + 0.00001 * K1G;
@@ -234,11 +305,14 @@ n = length(M1G);
 
 Minv = inv(M1G); % Yikes!
 
-A = [sparse(n, n), speye(n);
+McholL = chol(M1G).';
+Minv * K1G-McholL.'\(McholL\K1G) 
+
+N1 = [sparse(n, n), speye(n);
      -Minv * K1G, -Minv * D1G];
 
-B = [sparse(n, 2);
-     Minv * RB];
+G0 = [sparse(n, 2);
+     Minv * RB0];
 
 C = sparse(1, 2 * n); C(1, n - 1) = 1;
 
@@ -258,21 +332,31 @@ In3 = sparse(2*n^p, (2*n)^p);
 In3(:,idxs) = speye(2*n^p);
 
 N3 = [sparse(n, n^3), sparse(n,n^3);
-     -Minv * K3G, sparse(n,n^3)]*In3
+     -Minv * K3G, sparse(n,n^3)]*In3;
  
 % Construct Q
-Q = 0;
+Im = speye(2);
+G1 = [sparse(n,2*n), sparse(n,2*n);
+     Minv * RB1, sparse(n,2*n)];
+ 
+G2 = [sparse(n,2*n^2), sparse(n,2*n^2);
+     Minv * RB2, sparse(n,2*n^2)]*kron(In2,Im);
+ 
+G3 = [sparse(n,2*n^3), sparse(n,2*n^3);
+     Minv * RB3, sparse(n,2*n^3)]*kron(In3,Im);
+ 
 
 
 %% Format outputs
-f = {A,N2,N3}; 
-g = {B,Q};
+f = {N1,N2,N3}; 
+g = {G0,G1,G2,G3};
 h = {C};
 
-A = full(A);
-B = full(B);
+A = full(N1);
+B = full(G0);
 C = full(C);
 N = full(N2);
+Q = G1;
 
 
 end
