c2611376d1fa7c9f23b81ebce60ab6c6e5fec996
Nick Corbin <cnick1@vt.edu>
2023-10-31 09:56:50 -0700

    Fixed example12 typo from Fujimoto 2001 paper to 2005 model

--- examples/runExample12.m
+++ examples/runExample12.m
@@ -43,18 +43,56 @@ end
 [v] = approxPastEnergy(f, g, h, eta, degree, true);
 [w] = approxFutureEnergy(f, g, h, eta, degree, true);
 
+fprintf("Beginning comparisons with Fujimoto/Scherpen 2001/2005/2010:\n") 
+
+%% Comparison with Fujimoto 2001/2005 Example 1
+x = sym('x', [1, 2]).'; syms(x);
+
+LoSym =(36*x1^2 + 9*x2^2 + 18*x1^3*x2 + 18*x1*x2^3 + x1^6 + 6*x1^4*x2^2 + 9*x1^2*x2^4 + 4*x2^6)/ (1 + x1 ^ 4 + 2 * x1 ^ 2 * x2 ^ 2 + x2 ^ 4);
+[~,~,Lo] = approxPolynomialDynamics([1;1],[1;1],LoSym,x,degree);
+
+fprintf("\n  - Comparing our energy function with Fujimoto/Scherpen 2001/2005 Example 1:\n") 
+for i = 2:2:length(Lo)
+    fprintf("    > Degree %i: the largest difference in w%i is %.1e;", ...
+        i, i, norm(w{i} - kronMonomialSymmetrize(full(Lo{i}),2,i),'inf')) % Should be zero
+    fprintf(" the largest entry in v%i is %.1e;\n", ...
+        i, norm(v{i},'inf')) % Should be one for v2, zero else
+end
+fprintf("\n                             ->  Example 1 results match.\n\n")
+
+
+
 %% Compute the input-normal transformation approximation
-[sigma, Tin] = inputNormalTransformation(v, w, degree - 1, false);
+[sigma, Tin] = inputNormalTransformation(v, w, degree -1, true);
 
 %% Compute the output-diagonal transformation approximation, also giving the squared singular value functions
 [sigmaSquared, Tod] = outputDiagonalTransformation(v, w, Tin, diag(sigma), degree, true);
 
+fprintf("\n  - Comparing our energy function with Fujimoto/Scherpen 2010 Example 5:\n") 
+
+sigmaSquared;
+
+s1Sym = 2*sqrt((9+x1^4)/(1+x1^4))*x1;
+[~,~,s1] = approxPolynomialDynamics(1,1,s1Sym,x1,degree);
+[~,~,s2] = approxPolynomialDynamics(1,1,s1Sym/2,x1,degree);
+g1 = full(cell2mat(s1)); g1sq = conv(g1,g1);
+g2 = full(cell2mat(s2)); g2sq = conv(g2,g2);
+
+fprintf("    > Maximum distance between singular value functions squared:           %e \n",...
+    norm([g1sq(1:degree-1); g2sq(1:degree-1)] - sigmaSquared,'inf'))
 %% Plot the singular value functions
-n = length(f{1});
-z = linspace(- .5, .5, 51);
+
+z = linspace(-1, 1, 51);
 figure; hold on;
-for i = 1:n
-    plot(z, sqrt(polyval(flip(sigmaSquared(i, :)), z)))
-end
+
+plot(z, sqrt(polyval(flip(sigmaSquared(1, :)), z)), 'LineWidth', 1.5); hold on
+plot(z, polyval(flip(g1),z),'--', 'LineWidth', 1.5); hold on
+plot(z, 2*sqrt((9+z.^4)./(1+z.^4)),'k--', 'LineWidth', 1.5)
+
+plot(z, sqrt(polyval(flip(sigmaSquared(2, :)), z)), 'LineWidth', 1.5); hold on
+plot(z, polyval(flip(g2),z),'--', 'LineWidth', 1.5); hold on
+plot(z, sqrt((9+z.^4)./(1+z.^4)),'k--', 'LineWidth', 1.5)
+
+fprintf("\n                             ->  Example 5 results match.\n\n")
 
 end
