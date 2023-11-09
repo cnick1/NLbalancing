4d2e687a025a6f07e38ed5e58d74ff07a6671ecc
Nick Corbin <cnick1@vt.edu>
2023-11-08 20:30:32 -0800

    Made outputDiagonalization sparse/faster

--- outputDiagonalTransformation.m
+++ outputDiagonalTransformation.m
@@ -8,7 +8,7 @@ function [sigmaSquared, Tod] = outputDiagonalTransformation(v, w, Tin, Sigma, de
 %                 coefficients; these should already be in input-normal form.
 %       Tin     - cell array containing the input-normal transformation
 %                 coefficients.
-%       Sigma   - diagonal matrix of the Hankel singular values of the 
+%       Sigma   - diagonal matrix of the Hankel singular values of the
 %                 linearized system.
 %       degree  - desired degree of the computed transformation (default =
 %                 degree of energy functions - 1).
@@ -16,17 +16,17 @@ function [sigmaSquared, Tod] = outputDiagonalTransformation(v, w, Tin, Sigma, de
 %
 %   Outputs:
 %       sigmaSquared - an n×degree-1 matrix containing the coefficients of
-%                      the square of the singular value functions. The 
-%                      first column corresponds to the square of the Hankel 
-%                      singular values, the next column corresponds to the 
+%                      the square of the singular value functions. The
+%                      first column corresponds to the square of the Hankel
+%                      singular values, the next column corresponds to the
 %                      degree 1 coefficients, etc. These can be plotted
-%                      using polyval() (and flip()). 
-% 
-%                      Warning: Note that the singular value functions are 
+%                      using polyval() (and flip()).
+%
+%                      Warning: Note that the singular value functions are
 %                      NOT given by sigmaSquared.^(1/2). Use the function
 %                      utils/polySqrt() to get the coefficients of the
 %                      singular value functions.
-% 
+%
 %       Tod          - cell array containing the output-diagonal
 %                      transformation coefficients.
 %
@@ -88,64 +88,126 @@ Tod = cell(1, degree); Tod{1} = speye(n);
 [vtilde, wtilde] = transformEnergyFunctions(v, w, Tin, true); % Input-normal
 
 for k = 3:degree+1
-    [Nk] = equivalenceClassIndices(n, k);
-    
-    % \section{}
-    %$ Form input-normal equations coefficient matrix
-    CoeffMatrix = 2 * Nk;
-    
-    % Construct the RHS vector
-    RHS = [];
-    temp = zeros(size(Nk, 2), 1);
-    for i = 2:k - 2
-        j = k - i;
-        temp = temp + vec(Tod{j}.' * Tod{i});
-    end
-    RHS = [RHS; -Nk * temp];
-    
-    % \section
-    %% Form output-diagonal equations coefficient matrix
-    CoeffMatrix = [CoeffMatrix; 2 * Nk(n + 1:end, :) * kron(speye(n ^ (k - 1)), Sigma .^ 2)];
-    
-    temp = zeros(size(Nk(n + 1:end, :), 2), 1);
-    for i = 2:k - 2
-        j = k - i;
-        temp = temp + vec(Tod{j}.' * Sigma .^ 2 * Tod{i});
-    end
-    for i = 3:k
-        temp = temp + calTTv(Tod, i, k, wtilde{i}); % TODO: Accelerate this
-    end
-    RHS = [RHS; -Nk(n + 1:end, :) * temp];
-    
-    %% Form `flexibility' equations (Kronecker product repeated entries
-    [linclassidx] = referenceElementMap(n, k - 1);
-    
-    linclassidx(linclassidx) = []; % Basically remove the reference element so one is nonzero and the rest we eliminate
-    idxs = vec((n * (linclassidx - 1) + (1:n)).');
-    
-    %% Set extra parameter equation
-    % TODO: find best parameters; for now just solve "a" solution with mldivide
-    % parameterEqsRHS = 1;
-    % parameterEqsCoeff = zeros(1,n^k);
-    % parameterEqsCoeff(4) = 1;
-    
-    % CoeffMatrix(:,4) = [];
-    % indices(4) = [];
-    
-    % idxs = [idxs; 16]; % alternatively could solve minimum norm solution
-    
-    %% Assemble equations
-    CoeffMatrix(:, idxs) = []; % Kronecker flexibility
-    
-    % Form index set for the nonzero transformation components
-    indices = 1:n ^ k; indices(idxs) = [];
-    
-    Tod{k - 1} = zeros(n, n ^ (k - 1));
-    Tod{k - 1}(indices) = CoeffMatrix \ RHS;
-    
-    % Symmetrize
-    for i=1:n
-        Tod{k - 1}(i,:) = kronMonomialSymmetrize(Tod{k - 1}(i,:), n, k-1);
+    if true
+        [Nk] = equivalenceClassIndices(n, k);
+        Nkhat = Nk(n + 1:end, :);
+        
+        M1 = Nk;
+        M2Tr = zeros(n^k, size(Nkhat,1));
+        for i=1:size(Nkhat,1)
+            M2Tr(:,i) = vec(Sigma.^2  * reshape(Nkhat(i,:).',n,[]));
+        end
+        
+        tempSize = size(Nk,1);
+        Ainv = spdiags(1./sum(Nk,2),0,tempSize,tempSize); %diag(1./sum(Nk,2));
+        SchurAInv = spdiags(1./diag(M2Tr.'*(speye(n^k) - Nk.'*Ainv*Nk) * M2Tr), 0, tempSize-n,tempSize-n);
+        B = Nk * M2Tr; C = B.';
+        
+        M = 2*[M1; M2Tr.'];
+        MMTinv = 1/4*[Ainv + Ainv*B*SchurAInv*C*Ainv , -Ainv*B*SchurAInv ;
+            -SchurAInv*C*Ainv, SchurAInv];
+        
+        MPseudoInv = M.'*MMTinv;
+        
+        RHS = zeros(size(MPseudoInv,2),1);
+        % Construct the RHS vector
+        RHS = [];
+        temp = zeros(size(Nk, 2), 1);
+        for i = 2:k - 2
+            j = k - i;
+            temp = temp + vec(Tod{j}.' * Tod{i});
+        end
+        RHS = [RHS; -Nk * temp];
+        temp = zeros(size(Nk(n + 1:end, :), 2), 1);
+        for i = 2:k - 2
+            j = k - i;
+            temp = temp + vec(Tod{j}.' * Sigma .^ 2 * Tod{i});
+        end
+        for i = 3:k
+            temp = temp + calTTv(Tod, i, k, wtilde{i}); % TODO: Accelerate this
+        end
+        RHS = [RHS; -Nk(n + 1:end, :) * temp];
+        
+        % TODO: Could cut down size by using kronecker repeated entries
+                              
+        %% Solve equations
+        fprintf("Computing degree %i transformation... ", k-1)
+        tic
+        Tod{k - 1} = reshape(MPseudoInv * RHS,n,[]);             % Method 2
+        fprintf("%f seconds. \n", toc)
+    else
+        [Nk] = equivalenceClassIndices(n, k);
+        
+        %$ Form input-normal equations coefficient matrix
+        CoeffMatrix = 2 * Nk;
+        
+        % Construct the RHS vector
+        RHS = [];
+        temp = zeros(size(Nk, 2), 1);
+        for i = 2:k - 2
+            j = k - i;
+            temp = temp + vec(Tod{j}.' * Tod{i});
+        end
+        RHS = [RHS; -Nk * temp];
+        
+        % \section
+        %% Form output-diagonal equations coefficient matrix
+        CoeffMatrix = [CoeffMatrix; 2 * Nk(n + 1:end, :) * kron(speye(n ^ (k - 1)), Sigma .^ 2)];
+        
+        temp = zeros(size(Nk(n + 1:end, :), 2), 1);
+        for i = 2:k - 2
+            j = k - i;
+            temp = temp + vec(Tod{j}.' * Sigma .^ 2 * Tod{i});
+        end
+        for i = 3:k
+            temp = temp + calTTv(Tod, i, k, wtilde{i}); % TODO: Accelerate this
+        end
+        RHS = [RHS; -Nk(n + 1:end, :) * temp];
+        
+        %% Form `flexibility' equations (Kronecker product repeated entries
+        [linclassidx] = referenceElementMap(n, k - 1);
+        
+        linclassidx(linclassidx) = []; % Basically remove the reference element so one is nonzero and the rest we eliminate
+        idxs = vec((n * (linclassidx - 1) + (1:n)).');
+        
+        %% Set extra parameter equation
+        % TODO: find best parameters; for now just solve "a" solution with mldivide
+        % parameterEqsRHS = 1;
+        % parameterEqsCoeff = zeros(1,n^k);
+        % parameterEqsCoeff(4) = 1;
+        
+        % CoeffMatrix(:,4) = [];
+        % indices(4) = [];
+        
+        % idxs = [idxs; 16]; % alternatively could solve minimum norm solution
+        
+        %% Assemble equations
+        CoeffMatrix(:, idxs) = []; % Kronecker flexibility
+        
+        % Form index set for the nonzero transformation components
+        indices = 1:n ^ k; indices(idxs) = [];
+        
+        %% Solve equations
+        fprintf("Computing degree %i transformation... ", k-1)
+        disp(size(indices))
+        tic
+        Tod{k - 1} = zeros(n, n ^ (k - 1));
+        %     if k > 4
+        %         Tod{k - 1}(indices) = CoeffMatrix \ RHS;                   % Method 1
+        %     else
+        
+        Tod{k - 1}(indices) = pinv(full(CoeffMatrix)) * RHS;             % Method 2
+        
+        %         nullDir = rand(diff(size(CoeffMatrix)),1);                   % Method 3
+        %         Tod{k - 1}(indices) = CoeffMatrix \ RHS  + null(CoeffMatrix) * nullDir;
+        %     end
+        
+        % Symmetrize (needed when inverting the jacobian or something)
+        %     for i=1:n
+        %         Tod{k - 1}(i,:) = kronMonomialSymmetrize(Tod{k - 1}(i,:), n, k-1);
+        %     end
+        
+        fprintf("%f seconds. \n", toc)
     end
 end
 
@@ -162,11 +224,11 @@ for k = 2:degree+1
         fprintf("  - The largest entry in v%i is %.1e; ", k, max(abs(N * vbar{k}))) % Should be zero, other than the first time which is one
         fprintf("the largest off-diagonal entry in w%i is %.1e\n", k, max(abs(N(n + 1:end, :) * wbar{k}))) % Should be diagonal
         
-%         sigmaSquared{k - 1} = N(1:n, :) * wbar{k}; % Since the index set is already computed
+        %         sigmaSquared{k - 1} = N(1:n, :) * wbar{k}; % Since the index set is already computed
         sigmaSquared(:,k - 1) = N(1:n, :) * wbar{k}; % Since the index set is already computed
     else
         indexSet = linspace(1, n ^ k, n);
-%         sigmaSquared{k - 1} = wbar{k}(indexSet);
+        %         sigmaSquared{k - 1} = wbar{k}(indexSet);
         sigmaSquared(:,k - 1) = wbar{k}(indexSet);
     end
 end
@@ -178,6 +240,7 @@ function [N] = equivalenceClassIndices(n, k)
 %
 % Usage: [N] = equivalenceClassIndices(n,k)
 %
+% TODO: currently not very efficient
 
 %% Compute equivalence class index sets
 % We will compute a vector like [1 5 5 7 5 7 7 8]; the number in the vector
@@ -202,7 +265,8 @@ multiplicity = accumarray(uidx, 1);
 unique_values = unique_values(sortMonomials);
 
 % Construct the coefficient matrix
-N = (repmat(linclassidx.', length(unique_values), 1) == unique_values); % Binary equivalence class matrix
+% N = (repmat(linclassidx.', length(unique_values), 1) == unique_values); % Binary equivalence class matrix
+N = sparse((repmat(linclassidx.', length(unique_values), 1) == unique_values)); % Binary equivalence class matrix
 
 end
 
