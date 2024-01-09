f2b7b62d032c83caf3d13da100a13923d1147af1
Nick Corbin <cnick1@vt.edu>
2024-01-09 15:38:00 -0800

    Updated example 9 with control, need to now run on originial system

--- examples/getSystem9.m
+++ examples/getSystem9.m
@@ -52,7 +52,7 @@ if isempty(y0)
     vref= 0;
 else
     load(fullfile('examples', 'systemData',sprintf('system9_equilibria_N=%i.mat',N)))
-    vref = d(y0); vref = vref{1}; plot(y,vref)
+    vref = d(y0); vref = vref{1}; %plot(y,vref)
 end
 
 f{1} = eps*D2 + eye(n) - 3*diag(vref.^2);
--- examples/runExample9.m
+++ examples/runExample9.m
@@ -20,58 +20,75 @@ fprintf('Running Example 9\n')
 
 % Get system
 % for y0 = [-.5 -.25 0 .25 .5]
-eps = 0.01; N = 64; y0 = -0.5;
-[f, g, ~, ~, y] = getSystem9(eps, N, y0);
+eps = 0.01; N = 32; y0 = .25;
+[f, B, ~, ~, y] = getSystem9(eps, N, y0);
 fprintf("Maximum eigenvalue of A is %f; should be zero I think.\n",max(eigs(full(f{1}),N+1)))
 % end
 
 % Reference configuration (@ origin) -> v = v+vref
-vref = tanh((y-y0)/sqrt(2*eps));
+if isempty(y0)
+    vref=zeros(N+1,1);
+else
+    vref = tanh((y-y0)/sqrt(2*eps));
+end
+
+%% Construct stabilizing controller 
+Q = eye(N+1); R = eye(N+1); 
+[ValueFun] = pqr(f, B, Q, R);
+% u = @(z) zeros(N+1,1);
+u = @(z) (- R * B.' * kronPolyDerivEval(ValueFun, z).' / 2);
+% u = @(z) (- R * B.' * kronPolyDerivEval(ValueFun(1:2), z).' / 2);
+
 
 %% Solve PDE by Euler formula and plot results:
 % Initial condition
-v = .53*y + .47*sin(-1.5*pi*y);
-% v = tanh((y-(0.25))/sqrt(2*eps));
+v0 = .53*y + .47*sin(-1.5*pi*y);
+% v0 = tanh((y-(-0.125))/sqrt(2*eps*10));
+v = v0;
 
 % Shift initial condition 
 v=v-vref;
 % Time-stepping
-dt = min([.0001,50*N^(-4)/eps]); t = 0; 
+dt = min([.001,50*N^(-4)/eps]); t = 0; 
 tmax = 100; tplot = 2; nplots = round(tmax/tplot);
-plotgap = round(tplot/dt); dt = tplot/plotgap
+plotgap = round(tplot/dt); dt = tplot/plotgap;
 xx = -1:.025:1; vv = polyval(polyfit(y,v+vref,N),xx);
 plotdata = [vv; zeros(nplots,length(xx))]; tdata = t;
 for i = 1:nplots
     fprintf('%i',i)
     for n = 1:plotgap
         % t = t+dt; v = v + dt*(f{1}*v + f{2}*kron(v,v) + f{3}*kron(v,kron(v,v)));    % Euler
-        t = t+dt; v = v + dt*(f{1}*v + f{2}*kron(v,v) - v.^3);    % Euler
+        t = t+dt; v = v + dt*(f{1}*v + f{2}*kron(v,v) - v.^3 + B*u(v));    % Euler
     end
     vv = polyval(polyfit(y,v+vref,N),xx);
     plotdata(i+1,:) = vv; tdata = [tdata; t];
 end
-clf, subplot('position',[.1 .4 .8 .5])
+figure, subplot('position',[.1 .4 .8 .5])
 mesh(xx,tdata,plotdata), grid on, axis([-1 1 0 tmax -1 1]),
 view(-60,55), colormap(1e-6*[1 1 1]); xlabel x, ylabel t, zlabel u
 
 %% Analytical solution
-vref2 = tanh((xx+y0)/sqrt(2*eps));
 
 hold on
-plot3(xx,100*ones(size(xx)),vref2,"*")
+% plot3(xx,100*ones(size(xx)),polyval(polyfit(y,v0,N),xx),"ko")
+plot3(xx,100*ones(size(xx)),polyval(polyfit(y,v+vref,N),xx),"*")
 
+% load(fullfile('examples', 'systemData',sprintf('system9_equ ilibria_N=%i.mat',N)), 'd')
+% plot3(xx,100*ones(size(xx)),polyval(polyfit(y,cell2mat(d(y0)),N),xx),"g*")
+
+% view(180,0)
 return;
 
-figure
-hold on
-plot(xx,plotdata(end,:))
-plot(xx,vref2,"--")
-
-
-figure
-hold on
-plot(y,v,"*")
-plot(xx,vref2,"--")
+% figure
+% hold on
+% plot(xx,plotdata(end,:))
+% plot(xx,polyval(polyfit(y,vref,N),xx),"--")
+% 
+% 
+% figure
+% hold on
+% plot(y,v,"*")
+% plot(xx,vref2,"--")
 
 
 
