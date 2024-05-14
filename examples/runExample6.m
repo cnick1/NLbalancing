0759d87eb25560b4d3947eba227fe09d58833c4a
Nick Corbin <cnick1@vt.edu>
2024-05-13 23:22:32 -0700

    Added residual computations to runExample6

--- examples/runExample6.m
+++ examples/runExample6.m
@@ -5,12 +5,8 @@ function runExample6(exportData, x0)
 %   Usage:  runExample6(exportData, x0)
 %
 %   Inputs:
-%       exportData - Boolean variable to determine if
-%                         plots/data are exported
-%
-%   Outputs:
-%       v,w        - Coefficients of the past and future energy
-%                         function approximations, respectively
+%       exportData - Boolean to determine if data are exported
+%       x0         - Initial condition scaling factor
 %
 %   The value of eta is set below.
 %
@@ -34,11 +30,11 @@ eta = 0.1;
 degree = 3;
 
 % print results to command window as we run, then write to file
-fprintf('\n# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &   n  &     n^%d       &  CPU-sec  &  E_%d^+(x_0)     \n', degree, degree, degree);
+fprintf('\n# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &   n  &     n^%d       &  CPU-sec  &  E_%d^+(x_0)     &  RES_%d^+(x_0)     \n', degree, degree, degree, degree);
 
 nTest = [10, 10, 10, 10, 10, 1, 1, 1, 1];
 numEls = [1, 2, 4, 8, 16, 32, 64, 128, 180];
-nd = zeros(size(numEls)); times = zeros(size(numEls)); energies = zeros(size(numEls));
+nd = zeros(size(numEls)); times = zeros(size(numEls)); energies = zeros(size(numEls)); RES = zeros(size(numEls));
 if exportData, numTestCases = 7; else, numTestCases = 5; end % If running with 16GB ram, 7; if 256GB ram, 9
 for i=1:numTestCases
     fprintf(' %5d      &%4d  & ', numEls(i), 6 * numEls(i));
@@ -52,8 +48,11 @@ for i=1:numTestCases
     initialCondition = x0 * IC;
     wzInit = 0.5 * kronPolyEval(w, initialCondition, degree);
     
+    % Evaluate HJB residual
+    RES(i) = getFutureHJBResidual(f, g, h, eta, w, initialCondition);
+    
     % Print results to command window
-    fprintf(' %10.4e   & %8.2e  & %12.6e    \n', length(w{degree}), tt, wzInit);
+    fprintf(' %10.4e   & %8.2e  & %12.6e    &  %12.6e      \n', length(w{degree}), tt, wzInit, RES(i));
     
     % log results to write to file later
     nd(i)= length(w{degree}); times(i) = tt; energies(i) = wzInit;
@@ -63,10 +62,10 @@ if exportData
     [a, d] = getExpFit(times, numEls);
     fprintf('The exponent fit gives n^%f compared with n^%d. \nWriting data to plots/example6_convergenceData_d3.dat \n', d, degree)
     fileID = fopen('plots/example6_convergenceData_d3.dat', 'w');
-    fprintf(fileID, '# Table I Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &    n & n^%d           & CPU-sec   & E_%d^+(x_0)     &  exponentCoeff  &  exponentFit \n', degree, degree, degree);
+    fprintf(fileID, '# Table I Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &    n & n^%d           & CPU-sec   & E_%d^+(x_0)     &  RES_%d^+(x_0)     &  exponentCoeff  &  exponentFit \n', degree, degree, degree, degree);
     %print the header
     for i = 1:numTestCases
-        fprintf(fileID, '%5d       &%5d & %10.4e    & %8.2e  & %12.6e   &  %2.2f  &  %2.2f \n', numEls(i), 6 * numEls(i), nd(i), times(i), energies(i), a, d);
+        fprintf(fileID, '%5d       &%5d & %10.4e    & %8.2e  & %12.6e    &  %12.6e      &  %2.2f  &  %2.2f \n', numEls(i), 6 * numEls(i), nd(i), times(i), energies(i), RES(i), a, d);
     end
     fclose(fileID);
 end
@@ -75,12 +74,12 @@ end
 degree = 4;
 
 %print the header
-fprintf('\n# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &   n  &     n^%d       &  CPU-sec  &  E_%d^+(x_0)     \n', degree, degree, degree);
+fprintf('\n# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &   n  &     n^%d       &  CPU-sec  &  E_%d^+(x_0)     &  RES_%d^+(x_0)     \n', degree, degree, degree, degree);
 
 % compute and print the results
 nTest = [3, 3, 3, 1, 1];
 numEls = [1, 2, 4, 8, 16];
-nd = zeros(size(numEls)); times = zeros(size(numEls)); energies = zeros(size(numEls));
+nd = zeros(size(numEls)); times = zeros(size(numEls)); energies = zeros(size(numEls)); RES = zeros(size(numEls));
 if exportData, numTestCases = 5; else, numTestCases = 3; end
 for i=1:numTestCases
     fprintf(' %5d      &%4d  & ', numEls(i), 6 * numEls(i));
@@ -94,8 +93,11 @@ for i=1:numTestCases
     initialCondition = x0 * IC;
     wzInit = 0.5 * kronPolyEval(w, initialCondition, degree);
     
+    % Evaluate HJB residual
+    RES(i) = getFutureHJBResidual(f, g, h, eta, w, initialCondition);
+    
     % Print results to command window
-    fprintf(' %10.4e   & %8.2e  & %12.6e    \n', length(w{degree}), tt, wzInit);
+    fprintf(' %10.4e   & %8.2e  & %12.6e    &  %12.6e      \n', length(w{degree}), tt, wzInit, RES(i));
     
     % log results to write to file later
     nd(i)= length(w{degree}); times(i) = tt; energies(i) = wzInit;
@@ -104,17 +106,17 @@ if exportData
     [a, d] = getExpFit(times, numEls);
     fprintf('The exponent fit gives n^%f compared with n^%d. \nWriting data to plots/example6_convergenceData_d4.dat \n', d, degree)
     fileID = fopen('plots/example6_convergenceData_d4.dat', 'w');
-    fprintf(fileID, '# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &    n & n^%d           & CPU-sec   & E_%d^+(x_0)     &  exponentCoeff  &  exponentFit \n', degree, degree, degree);
+    fprintf(fileID, '# Table II Data\n# finite element beam model, convergence and scalability results; d=%d \nnumElements &    n & n^%d           & CPU-sec   & E_%d^+(x_0)     &  RES_%d^+(x_0)     &  exponentCoeff  &  exponentFit \n', degree, degree, degree, degree);
     for i = 1:numTestCases
-        fprintf(fileID, '%5d       &%5d & %10.4e    & %8.2e  & %12.6e   &  %2.2f  &  %2.2f \n', numEls(i), 6 * numEls(i), nd(i), times(i), energies(i), a, d);
+        fprintf(fileID, '%5d       &%5d & %10.4e    & %8.2e  & %12.6e    &  %12.6e      &  %2.2f  &  %2.2f \n', numEls(i), 6 * numEls(i), nd(i), times(i), energies(i), RES, a, d);
     end
     fclose(fileID);
 end
 
 %% Table III: convergence & scalability wrt d (n=18)
-numEl = 3;
+numEl = 4;
 
-fprintf('\n# Table III Data\n# finite element beam model, convergence and scalability results \n# numEls = %d   -->   n = %d \n   d   &  CPU-sec-2   & E_d^+(x_0)      \n', numEl, 6 * numEl);
+fprintf('\n# Table III Data\n# finite element beam model, convergence and scalability results \n# numEls = %d   -->   n = %d \n   d   &  CPU-sec-2   & E_d^+(x_0)     &  RES_d^+(x_0)     \n', numEl, 6 * numEl);
 
 % compute and print the results
 [f, g, h, IC] = getSystem6(numEl, 2);
@@ -125,15 +127,18 @@ initialCondition = x0 * IC;
 
 degrees = 2:6;
 nTest = [3, 3, 3, 3, 1];
-futureTimes = zeros(size(degrees)); futureEnergies = zeros(size(degrees));
+futureTimes = zeros(size(degrees)); futureEnergies = zeros(size(degrees)); RES = zeros(size(degrees));
 if exportData, numTestCases = 5; else, numTestCases = 3; end
 for i=1:numTestCases
     % Compute Future energy function
     tic; for j = 1:nTest(i), [w] = approxFutureEnergy(f, g, h, eta, degrees(i)); end, tt = toc / nTest(i);
     
     wzInit = 0.5 * kronPolyEval(w, initialCondition, degrees(i));
-    
-    fprintf('   %d   &   %8.2e   & %12.6e    \n', degrees(i), tt, wzInit);
+        
+    % Evaluate HJB residual
+    RES(i) = getFutureHJBResidual(f, g, h, eta, w, initialCondition);
+
+    fprintf('   %d   &   %8.2e   & %12.6e    & %12.6e    \n', degrees(i), tt, wzInit, RES(i));
     futureTimes(i) = tt; futureEnergies(i) = wzInit;
 end
 
@@ -149,7 +154,7 @@ if exportData
     fprintf(fileID, '# Table III Data\n# finite element beam model, convergence and scalability results \n# numEls = %d   -->   n = %d \n   d   & CPU-sec-2    &  E_d^+(x_0)     \n', numEl, 6 * numEl);
     
     for i = 1:numTestCases
-        fprintf(fileID, '   %d   &   %8.2e      & %12.6e    \n', degrees(i), futureTimes(i), futureEnergies(i));
+        fprintf(fileID, '   %d   &   %8.2e      & %12.6e    & %12.6e    \n', degrees(i), futureTimes(i), futureEnergies(i), RES(i));
     end
     fclose(fileID);
 end
@@ -165,4 +170,17 @@ X = [ones(length(logx), 1) logx']; % create matrix of x data with a column of on
 beta = X \ logy'; % solve for beta coefficients using linear least squares
 a = beta(1); % line y intercept (moves line up and down)
 d = beta(2); % calculate exponent d (slope of the line)
+end
+
+function RES = getFutureHJBResidual(f, g, h, eta, w, x)
+% Evaluate the Future HJB PDE at the point x; should be zero, so whatever
+% it is represents the error (residual)
+dEdX = 0.5 * kronPolyDerivEval(w, x);
+FofX = kronPolyEval(f, x);
+% GofX = g{1} + kronPolyEval(g(2:end), x);
+GofX = g{1}; lg = length(g); Im = speye(size(g{1},2));
+xk = 1; for k=2:lg; xk = kron(xk, x); GofX = g{k} * kron(xk, Im); end
+HofX = kronPolyEval(h, x);
+
+RES = dEdX*FofX - eta/2*dEdX*(GofX*GofX.')*dEdX.' + 0.5*(HofX.'*HofX);
 end
